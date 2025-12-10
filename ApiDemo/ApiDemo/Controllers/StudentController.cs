0238415974fc40bf183d44c7a8dcaa06376d330c
Badigi Aswartha Reddy <aswartha.b@Keka.com>
2025-12-10 18:03:13 +0530

    Student API

--- ApiDemo/ApiDemo/Controllers/StudentController.cs
+++ ApiDemo/ApiDemo/Controllers/StudentController.cs
@@ -3,27 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiDemo.Controllers
 {
     [ApiController] // Attributes
-    [Route("student")]   
-    public class StudentController : ControllerBase 
+    [Route("student")]
+    public class StudentController : ControllerBase
     {
 
         //  a new end point will have : http Method , pathname , code to execute
 
-        [HttpGet("Hello")] // Testing Route
-
-        public string Hello()
-        {
-            return "Hello from Student API"; 
-		}
-		// store the students 
-		public static List<Student> students = new List<Student>();
+        // store the students 
+        public static List<Student> students = new List<Student>();
 
 
         [HttpPost("add-student")] // post request to add the student
         public IActionResult AddStudent([FromBody] Student student)
         {
 
-            if(string.IsNullOrWhiteSpace(student.Name))
+            if (string.IsNullOrWhiteSpace(student.Name))
             {
                 return BadRequest(new ApiResponse<Student>
                 {
@@ -32,7 +26,7 @@ namespace ApiDemo.Controllers
                 });
             }
 
-            if(student.Age <= 0)
+            if (student.Age <= 0)
             {
                 return BadRequest(new ApiResponse<Student>
                 {
@@ -41,7 +35,7 @@ namespace ApiDemo.Controllers
                 });
             }
             // find and setting  the id manually 
-            int currentId = students.Count() > 0 ? students.Count() : 0;
+            int currentId = students.Count() > 0 ? students.Max(s => s.studentId) : 0;
             student.studentId = currentId + 1;
             students.Add(student);
             // succesfully added
@@ -55,10 +49,11 @@ namespace ApiDemo.Controllers
 
         [HttpGet("get-students")] // get all students
 
-		public IActionResult DisplayStudents()
+        public IActionResult DisplayStudents()
         {
 
-            if (students.Count == 0) {
+            if (students.Count == 0)
+            {
                 // data not found
                 return NotFound(new ApiResponse<List<Student>>
                 {
@@ -68,30 +63,30 @@ namespace ApiDemo.Controllers
             }
             // successfull retrive operations
             return Ok(new ApiResponse<List<Student>>
-			{
-				Message = "Students Fetched Succesfully",
-				Data = students
-			});
+            {
+                Message = "Students Fetched Succesfully",
+                Data = students
+            });
         }
 
         [HttpPatch("edit-student/{id}")]  // edit student by id by URl
-        public IActionResult EditStudentById(int id , [FromBody] Student student) 
+        public IActionResult EditStudentById(int id, [FromBody] Student student)
         {
-			if (students.Count == 0)
-			{
-				// data not found
-				return NotFound(new ApiResponse<List<Student>>
-				{
-					Message = "No Students Found",
-					Data = null
-				});
-			} 
-            
-			bool isExist = students.Any(stu => stu.studentId == id);
-            if (isExist==false)
+            if (students.Count == 0)
             {
-				// student not found
-				return NotFound(new ApiResponse<Student> 
+                // data not found
+                return NotFound(new ApiResponse<List<Student>>
+                {
+                    Message = "No Students Found",
+                    Data = null
+                });
+            }
+
+            bool isExist = students.Any(stu => stu.studentId == id);
+            if (isExist == false)
+            {
+                // student not found
+                return NotFound(new ApiResponse<Student>
                 {
                     Message = $"No Student Exist With The Id : {id} To Update",
                     Data = null
@@ -100,12 +95,12 @@ namespace ApiDemo.Controllers
 
             Student studentExist = students.First(s => s.studentId == id);
 
-            if ( string.IsNullOrWhiteSpace(student.Name) == false)
+            if (string.IsNullOrWhiteSpace(student.Name) == false)
             {
                 studentExist.Name = student.Name;
             }
 
-            if(student.Age > 0)
+            if (student.Age > 0)
             {
                 studentExist.Age = student.Age;
             }
@@ -119,30 +114,30 @@ namespace ApiDemo.Controllers
         }
 
         [HttpGet("get-student")] // get student by query param
-                                 
+
         public IActionResult GetStudentById([FromQuery] int id)
         {
-			// id is not provided
-			if (string.IsNullOrWhiteSpace(id.ToString()))
+            // id is not provided
+            if (string.IsNullOrWhiteSpace(id.ToString()))
             {
-                return BadRequest( new ApiResponse<Student>
+                return BadRequest(new ApiResponse<Student>
                 {
                     Message = "Id Is Required",
                     Data = null
                 });
             }
-			// id is negative or zero   
-			if (id <= 0)
+            // id is negative or zero   
+            if (id <= 0)
             {
-                return BadRequest( new ApiResponse<Student>
+                return BadRequest(new ApiResponse<Student>
                 {
                     Message = "Id Must Be Positive",
                     Data = null
                 });
             }
 
-			// not found cases
-			if (students.Count == 0)
+            // not found cases
+            if (students.Count == 0)
             {
                 // data not found
                 return NotFound(new ApiResponse<List<Student>>
@@ -152,23 +147,59 @@ namespace ApiDemo.Controllers
                 });
             }
             // Checking whether student is exist or not with given id
-            Student student = students.FirstOrDefault(student => student.studentId == id); 
-              if(student == null)
+            Student student = students.FirstOrDefault(student => student.studentId == id);
+            if (student == null)
             {
-                    return NotFound(new ApiResponse<Student>
-                    {
-                        Message = $"No Student Found With The Id : {id}",
-                        Data = null
-                    });
+                return NotFound(new ApiResponse<Student>
+                {
+                    Message = $"No Student Found With The Id : {id}",
+                    Data = null
+                });
             }
-			// successfull retrive operation
+            // successfull retrive operation
 
-			return Ok(new ApiResponse<Student>
-              {
-                  Message = "Student Fetched Succesfully",
-                  Data = student
-              });
+            return Ok(new ApiResponse<Student>
+            {
+                Message = "Student Fetched Succesfully",
+                Data = student
+            });
 
         }
-	}
+
+        [HttpDelete("delete-student/{id}")] // delete student by id from url    
+
+        public IActionResult DeleteStudentById(int id)
+        {
+
+            if (students.Count == 0)
+            {
+                // data not found
+                return NotFound(new ApiResponse<List<Student>>
+                {
+                    Message = "No Students Found",
+                    Data = null
+                });
+            }
+
+            Student student = students.FirstOrDefault(stu => stu.studentId == id);
+
+            if (student == null)
+            {
+                // student not found
+                return NotFound(new ApiResponse<Student>
+                {
+                    Message = $"No Student Exist With The Id : {id} To Delete",
+                    Data = null
+                });
+            }
+
+            students.Remove(student);
+
+            return Ok(new ApiResponse<Student>
+            {
+                Message = "Student Deleted Succesfully",
+                Data = student
+            });
+        }
+    }
 }
